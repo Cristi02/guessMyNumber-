7351ea39da7d1ea50e2c5600f86cbd19844b9641
Cristian Crudu <Cristian.Crudu@endava.com>
2022-10-04 08:48:16 +0300

    Fixed to work without modules

--- JS/Game.js
+++ JS/Game.js
@@ -1,4 +1,4 @@
-export default class Game {
+class Game {
     #theNumber;
     #lives;
     #maxNumberGenerated;
--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,10 @@
-import Game from "./Game.js";
 let game = null;
 const chooseRange = document.querySelector(".choose-range");
 const maxnumber = document.querySelector("#max-number");
 const gameElement = document.querySelector(".game");
 const playButton = document.querySelector("#play-button");
 const guessButton = document.querySelector("#take-a-guess");
+const guessNumber = document.querySelector("#guess-number");
 const answer = document.querySelector(".answer");
 
 const startGame = () => {
@@ -24,14 +24,15 @@ const guess = () => {
 
     const tryGuess = game.tryGuess(guessNumber);
 
-    if (tryGuess === 1)
-        answer.innerHTML = `The number is <span class='highlight'> greater </span> than ${guessNumber}`;
-    else if (tryGuess === -1)
-        answer.innerHTML = `The number is <span class='highlight'> less </span> than ${guessNumber}`;
+    let comparedToNumber = "";
+    if (tryGuess === 1) comparedToNumber = "greater";
+    else if (tryGuess === -1) comparedToNumber = "less";
     else {
         gameOver(true);
         return;
     }
+    answer.innerHTML = `The number is <span class='highlight'> ${comparedToNumber} </span> than ${guessNumber}`;
+
     if (game.lives === 0) gameOver();
 
     updateGraphics();
@@ -70,19 +71,22 @@ const updateGraphics = () => {
 
 const checkInput = (e) => {
     const value = Number.isInteger(e) ? e : e.target.value;
-    console.log(value);
     let errorMessage = "";
 
     if (!value) errorMessage = "Value must be an integer";
+
     try {
         parseInt(value);
     } catch (err) {
         console.log("Error cause", err.cause);
         errorMessage = "Value must be an integer";
     }
+
     if (+value < 0) errorMessage = "Value must be a positive integer";
+
     if (game && +value > game.maxNumber)
         errorMessage = `Value must not be greater than ${game.maxNumber}`;
+
     if (game && game.pastGuesses.find((x) => x === +value))
         errorMessage = `You have already tried ${value}`;
 
@@ -105,14 +109,11 @@ const setup = () => {
     maxnumber.value = 20;
 
     guessButton.addEventListener("click", guess);
-    document
-        .querySelector("#guess-number")
-        .addEventListener("input", checkInput);
-    document
-        .querySelector("#guess-number")
-        .addEventListener("keypress", (e) => {
-            if (e.key === "Enter") guess();
-        });
+    guessNumber.addEventListener("input", checkInput);
+    guessNumber.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") guess();
+    });
+
     document.querySelector("#restart").addEventListener("click", Game.restart);
 };
 
